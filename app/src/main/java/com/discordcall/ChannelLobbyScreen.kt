7d29c7b4f15ef0aa32bc3a78eb4db149ed593a70
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-03-11 23:16:45 -0300

    feat: upload files via Android Dev Tools

--- app/src/main/java/com/discordcall/ChannelLobbyScreen.kt
+++ app/src/main/java/com/discordcall/ChannelLobbyScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.*
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.*
